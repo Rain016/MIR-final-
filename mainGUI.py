df7f39431c50096739a47bd77b18150fda24176f
mainnolab <50371938+mainnolab@users.noreply.github.com>
2024-06-05 07:08:00 +0800

    Update the mainGUI.py

--- mainGUI.py
+++ mainGUI.py
@@ -53,10 +53,13 @@ def select_input_port():
     # Function to handle selection change
     def on_select(event):
         selected_port_name = selected_port.get()
+        entry_current_midi_input_port.delete(0, tk.END)  # Clear existing content
+        entry_current_midi_input_port.insert(tk.END, selected_port_name)  # Insert selected port name into Entry
         print_to_log(f"Selected MIDI input port: {selected_port_name}")
 
     # Bind event to selection change
     input_port_combobox.bind("<<ComboboxSelected>>", on_select)
+
 
 def select_output_port():
     # Get available MIDI output ports
@@ -78,6 +81,8 @@ def select_output_port():
     # Function to handle selection change
     def on_select(event):
         selected_port_name = selected_port.get()
+        entry_current_midi_output_port.delete(0, tk.END)  # Clear existing content
+        entry_current_midi_output_port.insert(tk.END, selected_port_name)  # Insert selected port name into Entry
         print_to_log(f"Selected MIDI output port: {selected_port_name}")
 
     # Bind event to selection change
@@ -96,29 +101,50 @@ def save_realtime_logs_to_file():
         with open(filepath, "w") as file:
             file.write(log_output.get("1.0", tk.END))
 
-def open_midi_recorder():
-    midi_recorder_window = tk.Toplevel(root)
-    midi_recorder_window.title("MIDI Recorder")
+def open_score_recorder():
+    score_recorder_window = tk.Toplevel(root)
+    score_recorder_window.title("Score Recorder")
 
     # Label and ScrolledText for Recorder Terminal
-    label_terminal_recorder = tk.Label(midi_recorder_window, text='MIDI Recorder Terminal')
+    label_terminal_recorder = tk.Label(score_recorder_window, text='Score Recorder Terminal')
     label_terminal_recorder.grid(row=0, column=0, padx=10, pady=5)
 
-    recorder_terminal = scrolledtext.ScrolledText(midi_recorder_window, wrap=tk.WORD, width=60, height=30)
+    recorder_terminal = scrolledtext.ScrolledText(score_recorder_window, wrap=tk.WORD, width=60, height=30)
     recorder_terminal.grid(row=1, column=0, padx=10, pady=5)
 
     # Buttons
-    button_run = tk.Button(midi_recorder_window, text="Run", width=15)
+    button_run = tk.Button(score_recorder_window, text="Run", width=15)
     button_run.grid(row=2, column=0, padx=5, pady=10, sticky='ew')
 
-    button_stop = tk.Button(midi_recorder_window, text="Stop", width=15)
+    button_stop = tk.Button(score_recorder_window, text="Stop", width=15)
     button_stop.grid(row=3, column=0, padx=5, pady=10, sticky='ew')
 
-    button_save = tk.Button(midi_recorder_window, text="Save", width=15)
+    button_save = tk.Button(score_recorder_window, text="Save", width=15)
     button_save.grid(row=4, column=0, padx=5, pady=10, sticky='ew')
 
-    button_close = tk.Button(midi_recorder_window, text="Close", width=15, command=midi_recorder_window.destroy)
+    button_close = tk.Button(score_recorder_window, text="Close", width=15, command=score_recorder_window.destroy)
     button_close.grid(row=5, column=0, padx=5, pady=10, sticky='ew')
+
+def open_score_player():
+    score_player_window = tk.Toplevel(root)
+    score_player_window.title("Score Player")
+    
+    # Label and ScrolledText for Player Terminal
+    label_terminal_recorder = tk.Label(score_player_window, text='Score Recorder Terminal')
+    label_terminal_recorder.grid(row=0, column=0, padx=10, pady=5)
+
+    recorder_terminal = scrolledtext.ScrolledText(score_player_window, wrap=tk.WORD, width=60, height=30)
+    recorder_terminal.grid(row=1, column=0, padx=10, pady=5)
+
+    # Buttons
+    button_run = tk.Button(score_player_window, text="Play", width=15)
+    button_run.grid(row=2, column=0, padx=5, pady=10, sticky='ew')
+
+    button_stop = tk.Button(score_player_window, text="Stop", width=15)
+    button_stop.grid(row=3, column=0, padx=5, pady=10, sticky='ew')
+
+    button_close = tk.Button(score_player_window, text="Close", width=15, command=score_player_window.destroy)
+    button_close.grid(row=4, column=0, padx=5, pady=10, sticky='ew')
 
 root = tk.Tk()
 root.title("你的音樂好夥伴")
@@ -143,7 +169,8 @@ menu_bar.add_cascade(label="Output Port", menu=output_port_menu)
 
 # Tools Menu
 tools_menu = tk.Menu(menu_bar, tearoff=0)
-tools_menu.add_command(label="MIDI Recorder", command=open_midi_recorder)
+tools_menu.add_command(label="MIDI Recorder", command=open_score_recorder)
+tools_menu.add_command(label="MIDI Player", command=open_score_player)
 menu_bar.add_cascade(label="Tools", menu=tools_menu)
 
 # Help Menu
@@ -172,6 +199,7 @@ terminal_output2.grid(row=1, column=1, padx=10, pady=10)
 # Label and ScrolledText for Log Output
 label_terminal_recorder = tk.Label(root, text='Realtime Monitor')
 label_terminal_recorder.grid(row=0, column=2, padx=10, pady=5)
+
 log_output = scrolledtext.ScrolledText(root, wrap=tk.WORD, width=60, height=30)
 log_output.config(state=tk.NORMAL)
 log_output.grid(row=1, column=2, padx=10, pady=5)
@@ -183,28 +211,53 @@ button_open1.grid(row=2, column=0, padx=5, pady=10, sticky='ew')
 button_open2 = tk.Button(root, text="Load Sheet Music to Accompaniment Monitor", width=30, command=open_file_to_terminal2)
 button_open2.grid(row=2, column=1, padx=5, pady=10, sticky='ew')
 
+button_save_to_file = tk.Button(root, text="Save Realtime Monitor to File", width=30, command=save_realtime_logs_to_file)
+button_save_to_file.grid(row=2, column=2, padx=5, pady=10, sticky='ew')
+
 button_clear1 = tk.Button(root, text="Clear Melody Monitor", width=20, command=clear_terminal1)
 button_clear1.grid(row=3, column=0, padx=5, pady=10, sticky='ew')
 
 button_clear2 = tk.Button(root, text="Clear Accompaniment Monitor", width=20, command=clear_terminal2)
 button_clear2.grid(row=3, column=1, padx=5, pady=10, sticky='ew')
 
-button_play = tk.Button(root, text="Play", width=15, command=play_midi)
-button_play.grid(row=5, column=1, padx=5, pady=10, sticky='ew')
-
-button_stop = tk.Button(root, text="Stop", width=15, command=stop_midi)
-button_stop.grid(row=6, column=1, padx=5, pady=10, sticky='ew')
-
-button_reset_midi = tk.Button(root, text="Reset MIDI", width=15, command=reset_midi)
-button_reset_midi.grid(row=4, column=1, padx=5, pady=9, sticky='ew')
-
-button_exit = tk.Button(root, text="Exit", width=15, command=root.quit)
-button_exit.grid(row=7, column=1, padx=5, pady=10, sticky='ew')
-
-button_save_to_file = tk.Button(root, text="Save Realtime Monitor to File", width=30, command=save_realtime_logs_to_file)
-button_save_to_file.grid(row=2, column=2, padx=5, pady=10, sticky='ew')
-
 button_clear3 = tk.Button(root, text="Clear Realtime Monitor", width=25, command=clear_realtime_logs)
 button_clear3.grid(row=3, column=2, padx=5, pady=10, sticky='ew')
+
+button_reset_midi = tk.Button(root, text="Reset MIDI", width=15, command=reset_midi)
+button_reset_midi.grid(row=4, column=0, padx=5, pady=9, sticky='ew')
+
+button_play = tk.Button(root, text="Play", width=15, command=play_midi)
+button_play.grid(row=4, column=1, padx=5, pady=10, sticky='ew')
+
+button_score_recorder = tk.Button(root, text="Score Recorder", width=15, command=open_score_recorder)
+button_score_recorder.grid(row=4, column=2, padx=5, pady=10, sticky='ew')
+
+button_stop = tk.Button(root, text="Stop", width=15, command=stop_midi)
+button_stop.grid(row=5, column=1, padx=5, pady=10, sticky='ew')
+
+button_score_player = tk.Button(root, text="Score Player", width=15, command=open_score_player)
+button_score_player.grid(row=5, column=2, padx=5, pady=10, sticky='ew')
+
+button_exit = tk.Button(root, text="Exit", width=15, command=root.quit)
+button_exit.grid(row=6, column=1, padx=5, pady=10, sticky='ew')
+
+#Frame 1
+frame1 = tk.Frame(root)
+frame1.grid(row=5, column=0, padx=10, pady=10)
+
+label_current_midi_input_port = tk.Label(frame1, text="Current MIDI Input Port : ")
+label_current_midi_input_port.grid(row=0, column=0)
+
+entry_current_midi_input_port = tk.Entry(frame1, width=40)
+entry_current_midi_input_port.grid(row=0, column=1)
+
+frame2 = tk.Frame(root)
+frame2.grid(row=6, column=0, padx=10, pady=10)
+
+label_current_midi_output_port = tk.Label(frame2, text="Current MIDI Output Port : ")
+label_current_midi_output_port.grid(row=0, column=2)
+
+entry_current_midi_output_port = tk.Entry(frame2, width=40)
+entry_current_midi_output_port.grid(row=0, column=3,)
 
 root.mainloop()
